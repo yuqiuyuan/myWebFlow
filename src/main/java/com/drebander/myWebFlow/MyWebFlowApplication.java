666044509dab9a0ba81c1060551738365f840b27
yuqiuyuan <derbander@163.com>
2019-06-18 23:32:23 +0800

    Initial commit

---
+++ src/main/java/com/drebander/myWebFlow/MyWebFlowApplication.java
@@ -0,0 +1,13 @@
+package com.drebander.myWebFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebFlowApplication.class, args);
+	}
+
+}
